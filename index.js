c921129c7229d06767b8f1f8620f8a138a3c37e5
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2023-07-24 09:19:14 +0530

    DiceGame

---
+++ index.js
@@ -0,0 +1,7 @@
+let randomNumber1 =Math.ceil(Math.random()*6);
+
+var randomNumber = "dice" + randomNumber1 +".png";
+var randomIMageSource = randomIMageSource;
+
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("")
